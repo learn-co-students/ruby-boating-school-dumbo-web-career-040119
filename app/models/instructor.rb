fb5dabf4908c8d6cacad86b25c1614cef470f662
Mallory Woods <malvwoods@gmail.com>
2019-04-09 11:23:27 -0400

    wrote and tested first method for Student class

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,22 @@
 class Instructor
 
+  attr_reader :name
+  @@all = []
+# * initialize with a name
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  # * `Instructor.all` should return all instructors
+  def self.all
+    @@all
+  end
+
 end
+
+
+
+
+# * `Instructor#pass_student` should take in a student instance and test name. If there is a `BoatingTest` whose name and student match the values passed in, this method should update the status of that BoatingTest to 'passed'. If there is no matching test, this method should create a test with the student, that boat test name, and the status 'passed'. Either way, it should return the `BoatingTest` instance.
+# * `Instructor#fail_student` should take in a student instance and test name. Like `#pass_student`, it should try to find a matching `BoatingTest` and update its status to 'failed'. If it cannot find an existing `BoatingTest`, it should create one with the name, the matching student, and the status 'failed'.
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,27 @@
 class Student
 
+  attr_reader :fist_name
+  @@all = []
+  # * should initialize with `first_name`
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  # * `Student.all` should return all of the student instances
+  def self.all
+    @@all
+  end
+
+  # * `Student#add_boating_test` should initialize a new boating test with a Student (Object), a boating test name (String), a boating test status (String), and an Instructor (Object)
+  def add_boating_test(test_name, status, instructor)
+    BoatingTest.new(self, test_name, status, instructor)
+  end
+
 end
+
+
+
+
+# * `Student.find_student` will take in a first name and output the student (Object) with that name
+# * `Student#grade_percentage` should return the percentage of tests that the student has passed, a Float (so if a student has passed 3 / 9 tests that they've taken, this method should return the Float `33.33`)
