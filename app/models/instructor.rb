0473ea952457319097671d7945d516295476a224
Adam Kearney <aklives@hotmail.com>
2019-04-09 16:52:39 -0400

    finally passed

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,56 @@
 class Instructor
 
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pass_student(student, test_name)
+    pass_test = BoatingTest.all.select do |boat_test|
+                 boat_test.instructor == self
+                end
+    pass_test.select do |test|
+      if test.student == student && test.test_name == test_name
+         test.test_status = "passed"
+         return test
+      else
+        new_pass = student.add_boating_test(test.test_name, "passed", self)
+        return new_pass
+
+      end
+    end
+  end
+
+  def fail_student(student, test_name)
+    fail_test = BoatingTest.all.select do |boat_test|
+                 boat_test.instructor == self
+                end
+    fail_test.select do |test|
+      if test.student == student && test.test_name == test_name
+         test.test_status = "failed"
+         return test
+      else
+        new_pass = student.add_boating_test(test.test_name, "failed", self)
+        return new_pass
+
+      end
+    end
+  end
+
+
+
+
+    #if pass_test.student == student.first_name
+
+
+
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,45 @@
 class Student
 
+  attr_reader :first_name
+
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(test_name, test_status, instructor)
+    BoatingTest.new(self, test_name, test_status, instructor)
+  end
+
+  def self.find_student(first_name)
+    self.all.select do |student|
+      student.first_name == first_name
+    end
+  end
+
+  def grade_percentage
+    arr_students_tests = BoatingTest.all.select do |test|
+      test.student == self
+    end
+
+    passed_tests = arr_students_tests.select do |test|
+      test.test_status == "passed"
+    end
+
+    failed_tests = arr_students_tests.select do |test|
+      test.test_status == "failed"
+    end
+
+    ratio = passed_tests.length.to_f / arr_students_tests.length
+    ratio *= 100
+  end
+
+
 end
