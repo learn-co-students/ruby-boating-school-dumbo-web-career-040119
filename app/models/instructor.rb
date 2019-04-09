3ecf7ee786f96abacd3b5624c95388e0f21ec62e
cogilvy <chrisogilvy@cox.net>
2019-04-09 18:58:28 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -13,30 +13,32 @@ class Instructor
   end
 
   def pass_student(student, test_name)
-    truthy = BoatingTest.all.find do |test|
+    test_to_pass = BoatingTest.all.find do |test|
       test.student.first_name == student.first_name && test.test_name == test_name
     end
 
-    if truthy
-      truthy.status = "passed"
+    # If test_to_pass exists...
+    if test_to_pass
+      test_to_pass.status = "passed"
     elsif
-      truthy = BoatingTest.new(student, test_name, "passed", self)
+      test_to_pass = BoatingTest.new(student, test_name, "passed", self)
     end
-    truthy
+    test_to_pass
   end
 
 
   def fail_student(student, test_name)
-    truthy = BoatingTest.all.find do |test|
+    test_to_fail = BoatingTest.all.find do |test|
       test.student.first_name == student.first_name && test.test_name == test_name
     end
 
-    if truthy
-      truthy.status = "failed"
+    # # If test_to_fail exists...
+    if test_to_fail
+      test_to_fail.status = "failed"
     elsif
-      truthy = BoatingTest.new(student, test_name, "failed", self)
+      test_to_fail = BoatingTest.new(student, test_name, "failed", self)
     end
-    truthy
+    test_to_fail
   end
 
 end
