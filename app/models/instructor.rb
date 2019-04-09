eb3d6e295ab137923fd8dbc8b9654ed876fd3659
cogilvy <chrisogilvy@cox.net>
2019-04-09 18:45:08 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,42 @@
 class Instructor
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pass_student(student, test_name)
+    truthy = BoatingTest.all.find do |test|
+      test.student.first_name == student.first_name && test.test_name == test_name
+    end
+
+    if truthy.class == BoatingTest
+      truthy.status = "passed"
+    elsif
+      truthy = BoatingTest.new(student, test_name, "passed", self)
+    end
+    truthy
+  end
+
+
+  def fail_student(student, test_name)
+    truthy = BoatingTest.all.find do |test|
+      test.student.first_name == student.first_name && test.test_name == test_name
+    end
+
+    if truthy.class == BoatingTest
+      truthy.status = "failed"
+    elsif
+      truthy = BoatingTest.new(student, test_name, "failed", self)
+    end
+    truthy
+  end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,64 @@
+require 'pry'
 class Student
+  attr_reader :first_name
+
+  @@all = []
+
+  # should initialize with first_name
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  # Student.all should return all of the student instances
+  def self.all
+    @@all
+  end
+
+  # Student#add_boating_test should initialize a new boating test with a Student
+  # (Object), a boating test name (String), a boating test status (String), and
+  # an Instructor (Object).
+  def add_boating_test(test_name, status, instructor)
+    BoatingTest.new(self, test_name, status, instructor)
+  end
+
+  # Student.find_student will take in a first name and output the student
+  # (Object) with that name. Look through all students, self.all, and select
+  # only the student whose .first_name value == first_name passed in as arg.
+  # Will output an array!
+  def self.find_student(first_name)
+    self.all.select do |student|
+      student.first_name == first_name
+    end
+  end
+
+  # Student #grade_percentage should return the percentage of tests that the
+  # student has passed, a Float (so if a student has passed 3 / 9 tests that
+  # they've taken, this method should return the Float 33.33)
+  def grade_percentage
+    # This is the total number of tests a student (self) has taken.
+    student_tests = BoatingTest.all.select do |test|
+      test.student == self
+    end
+
+    # From the total number of tests taken by that student (self), we just want
+    # the one's that have passed.
+    passed_test = student_tests.select do |test|
+      test.status == "passed"
+    end
+    # From the total number of tests taken by that student (self), we just want
+    # the one's that have failed.
+    failed_test = student_tests.select do |test|
+      test.status == "failed"
+    end
+
+    # Now we need to find the percent of tests that this student (self) has
+    # passed compared to the total number of tests they have taken.
+    # Multiply by 100 to get a % value instead of decimal.
+    # ratio = (passed_test.length.to_f)/(student_tests.length)
+    # ratio * 100         SAME AS BELOW
+    ratio = "%.2f" % (passed_test.length.fdiv(student_tests.length)*100).to_s + "%"
+    # The "%.2f"
+  end
 
 end
