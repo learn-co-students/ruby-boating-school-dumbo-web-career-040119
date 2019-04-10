20edb7c3e317cc0d3cc4d21f30e6c3887186546d
github username <github email address>
2019-04-10 13:03:11 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,25 @@
 class Instructor
 
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+
+    @@all << self
+  end
+
+  def get_test(student, test_name)
+    BoatingTest.all.find {|test| test.student.first_name == student && test.name == test_name && test.instructor == self}
+  end
+
+  def pass_student(student, test_name)
+    get_test(student, test_name).status = "passed"
+  end
+
+  def fail_student(student, test_name)
+    get_test(student, test_name).status = "failed"
+  end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,34 @@
 class Student
 
+  attr_reader :first_name
+
+  @@all =[]
+
+  def initialize(first_name)
+    @first_name = first_name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_student(first_name)
+    self.all.select {|student| student.first_name == first_name}
+  end
+
+  def add_boating_test(instructor, name, status)
+    BoatingTest.new(self, instructor, name, status)
+  end
+
+  def grade_percentage
+    grade = BoatingTest.all.select {|test| test.student == self}
+    grade.map!(&:status)
+
+    passed_tests = grade.select {|test| test == "passed"}
+
+    passed_tests.length/grade.length.to_f * 100
+  end
+
 end
